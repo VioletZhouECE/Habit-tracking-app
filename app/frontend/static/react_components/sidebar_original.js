b6315a65f834bce26ef2d88004e1c0858a901e8c
周纯 <zhouchun@zhouchuns-MacBook-Air.local>
2020-04-05 14:04:30 -0400

    edit icons

--- app/frontend/static/react_components/sidebar_original.js
+++ app/frontend/static/react_components/sidebar_original.js
@@ -12,36 +12,17 @@ function sidebarOriginal() {
                 <li className="active">
                     <a href = "#"><i className="fas fa-plus-circle fa-2x"></i></a>
                 </li>
-                <li id = "nav_work_flow" className = "mt-2 pl5">
-                    <a href = "#"><i className="fas fa-tasks"></i></a><a href = "#" className = "pl-3">Work Flow</a>
+                <li id = "nav_work_flow" className = "mt-2">
+                    <a href = "#"><i className="fas fa-tasks"></i></a><a href = "#" className = "pl-3">Plans</a>
                 </li>
-                <li id = "nav_statistics" className = "mt-2 pl5">
-                    <a href = "#"><i className="fas fa-chart-line"></i></a><a href="#stat_submenu" data-toggle="collapse" className="dropdown-toggle pl-3">Statistics</a>
-                    <ul className="collapse list-unstyled" id="stat_submenu">
-                        <li>
-                            <a href="#">By tasks</a>
-                        </li>
-                        <li>
-                            <a href="#">Weekly Summary</a>
-                        </li>
-                        <li>
-                            <a href="#">Monthly Summary</a>
-                        </li>
-                    </ul>
+                <li id = "nav_statistics" className = "mt-2">
+                    <a href = "#"><i class="fas fa-calendar-minus"></i></a><a href = "#" className = "pl-3">Today</a>
                 </li>
-                <li id = "nav_create_plans" className= "mt-2 pl5">
-                    <a href = "#"><i className="fas fa-edit"></i></a><a href="#plan_submenu" data-toggle="collapse" className="dropdown-toggle pl-3">Create Plans</a>
-                    <ul className = "collapse list-unstyled" id = "plan_submenu">
-                        <li>
-                            <a href = "#">Saving plan</a>
-                        </li>
-                        <li>
-                            <a href = "#">Expense plan</a>
-                        </li>
-                    </ul>
+                <li id = "nav_create_plans" className= "mt-2">
+                    <a href = "#"><i className="fas fa-user-circle"></i></a><a href = "#" className = "pl-3">Me</a>
                 </li>
-                <li id = "nav_achievements" className = "mt-2 pl5">
-                    <a href = "#"><i className ="fas fa-trophy"></i></a><a href = "#" className= "pl-3 mt-2">Achievements</a>
+                <li id = "nav_achievements" className = "mt-2">
+                    <a href = "#"><i className ="fas fa-comment"></i></a><a href = "#" className= "pl-3">Community</a>
                 </li>
             </ul>
         </nav>
