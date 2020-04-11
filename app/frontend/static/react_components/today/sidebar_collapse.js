7c86dabab9e01535bd60867a5059f034ccfb2238
VioletZhouECE <x375zhou@uwaterloo.ca>
2020-04-11 11:39:43 -0400

    add react router (not working version)

--- app/frontend/static/react_components/today/sidebar_collapse.js
+++ app/frontend/static/react_components/today/sidebar_collapse.js
@@ -1,16 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {Link} from "react-router-dom";
 
 function sidebarCollapse() {
     return (
-        <nav id="sidebar" className = "pl-3 pt-2" style = {{maxWidth:"60px", minWidth:"60px"}}>
+        <nav id="sidebar_collapse" className = "pl-3 pt-2" style = {{maxWidth:"60px", minWidth:"60px"}}>
             <div className="sidebar-header">
                 <h3>VZ</h3>
             </div>
             <ul className="list-unstyled components">
                 <p>Time</p>
                 <li className="active">
-                    <a href = "#"><i className="fas fa-plus-circle fa-2x"></i></a>
+                    <Link to='/create_a_plan'><i className="fas fa-plus-circle fa-2x"></i></Link>
                 </li>
                 <li id = "nav_work_flow" className = "mt-2 pl5">
                     <a href = "#"><i className="fas fa-tasks"></i></a>
