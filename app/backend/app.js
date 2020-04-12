a7d74aa94f8ea84e95c5fd886691f13b3ef42477
VioletZhouECE <x375zhou@uwaterloo.ca>
2020-04-11 21:46:31 -0400

    config backend

--- app/backend/app.js
+++ app/backend/app.js
@@ -1,11 +1,12 @@
-import express from 'express'
+const express = require('express')
 
 app = express();
 
-app.set('view-engine', 'ejs');
+//app.set('view-engine', 'ejs');
+app.use(express.static('../frontend/public'));
 
-app.get('/', (req, res)=> {
-    res.render('../frontend/template/home.js')
+app.get('/*', (req, res)=> {
+    res.render('/template/home.html')
 });
 
-app.listen(3000);
+app.listen(3000, '127.0.0.1');
