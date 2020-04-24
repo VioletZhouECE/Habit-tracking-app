f7dbb4135418264868dc05a4bf931b87e2fd464e
VioletZhouECE <x375zhou@uwaterloo.ca>
2020-04-24 08:55:25 -0400

    get create a plan page layout to work

--- app/backend/app.js
+++ app/backend/app.js
@@ -1,12 +1,16 @@
 const express = require('express')
+const path = require('path')
 
 app = express();
 
-//app.set('view-engine', 'ejs');
-app.use(express.static('../frontend/public'));
+//serve home.html
+app.use(express.static("/Users/zhouchun/time_bank_app_node/app/frontend/template"));
 
-app.get('/*', (req, res)=> {
-    res.render('/template/home.html')
-});
+//serve all static css, javascript and bundle files
+app.use(express.static("/Users/zhouchun/time_bank_app_node/app/frontend"));
+
+app.get( '/*', (req, res) => {
+    res.redirect("/Users/zhouchun/time_bank_app_node/app/frontend/template/home.html")
+})
 
 app.listen(3000, '127.0.0.1');
