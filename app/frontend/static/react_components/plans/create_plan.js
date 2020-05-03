d6801c3b58ed46b8cdded0a0658feae23ed44efe
VioletZhouECE <x375zhou@uwaterloo.ca>
2020-05-03 12:37:57 -0400

    add color selector

--- app/frontend/static/react_components/plans/create_plan.js
+++ app/frontend/static/react_components/plans/create_plan.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import FontIconPicker from '@fonticonpicker/react-fonticonpicker';
-//import '@fonticonpicker/react-fonticonpicker/dist/fonticonpicker.base-theme.react.css';
-//import '@fonticonpicker/rnpmeact-fonticonpnpicker/dist/fonticonpicker.material-theme.react.css';
+
+const icons = ['fipicon-angle-left', 'fipicon-angle-right', 'fipicon-angle-up', 'fipicon-angle-down'];
+const colors = ['cyan', '']
 
 class CreatePlan extends React.Component{
     constructor(props){
         super(props);
         this.state = {
+            theme : "bluegrey",
             icon :'fipicon-angle-left',
         }
 
@@ -21,9 +23,22 @@ class CreatePlan extends React.Component{
     render(){
         return (
             <div className = "create-a-plan">
-                <p>Name your habit</p>
+                <p className="pl-1">Name your habit:</p>
                 <input className= "habit-name" placeholder = "my plan"></input>
-                <FontIconPicker icons={['fipicon-angle-left', 'fipicon-angle-right', 'fipicon-angle-up', 'fipicon-angle-down']} value={this.state.icon} onChange={this.handleSelectIcon}></FontIconPicker>
+                <div className="d-flex">
+                    <div className="icon-selection">
+                        <p className="pt-3"> Pick an icon: </p>
+                        <FontIconPicker icons={['fipicon-angle-left', 'fipicon-angle-right', 'fipicon-angle-up', 'fipicon-angle-down']} value={this.state.icon} onChange={this.handleSelectIcon} theme={this.state.theme}></FontIconPicker>
+                    </div>
+                    <div className="icon-color-selection pt-3 pl-5">
+                        <p> Pick a color: </p>
+                        <div className="d-flex pt-3">
+                            <span><i className="fas fa-circle fa-2x" style={{color:"cyan"}}></i></span>
+                            <span><i className="fas fa-circle fa-2x pl-2" style={{color:"bluegrey"}}></i></span>
+                            <span><i className="fas fa-circle fa-2x pl-2" style={{color:"purple"}}></i></span>
+                        </div>
+                    </div>
+                </div>
                 <div className = "dropdown-menu"></div>
                 <p>Add a tag</p>
                 <p>Choose frequency</p>
